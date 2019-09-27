6038bc5ad8e14be8ad461492df448f0d42e0c1a6
Ismael Nascimento <contato@ismaelnascimento.com>
2019-09-27 15:26:14 -0300

    initial commit

---
+++ Assets/_Project/Scripts/AddresableLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public static class AddressableLoader
+{
+    public static IEnumerator Initialize<T>(string assetLabel, List<T> assets)
+    {
+        yield return Addressables.LoadAssetsAsync<T>(assetLabel, op =>
+        {
+            assets.Add(op);
+        });
+    }
+
+    public static IEnumerator InitCreate<T>(string assetLabel, List<T> assets) where T : Object
+    {
+        yield return Addressables.LoadAssetsAsync<T>(assetLabel, op =>
+        {
+            assets.Add(Object.Instantiate(op));
+        });
+    }
+
+    public static IEnumerator InitCreateDeactivate<T>(string assetLabel, List<T> assets) where T : Object
+    {
+        yield return Addressables.LoadAssetsAsync<T>(assetLabel, op =>
+        {
+            assets.Add(Object.Instantiate(op));
+        });
+
+        foreach (var asset in assets)
+        {
+            GameObject.Find(asset.name).gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/AddressableManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+
+public class AddressableManager : MonoBehaviour
+{
+	#region VARIABLES
+
+	[SerializeField] private AssetLabelReference assetLabelReference;
+    [SerializeField] private GameObject animation;
+    private List<IResourceLocation> cubes;
+
+	#endregion
+
+	#region MONOBEHAVIOUR_METHODS
+
+	private void Start()
+	{
+		Addressables.LoadResourceLocationsAsync(assetLabelReference.labelString).Completed += AddressableManager_Completed;
+	}
+
+	#endregion
+
+
+	#region PRIVATE_METHODS
+
+	private void AddressableManager_Completed(AsyncOperationHandle<IList<IResourceLocation>> operation)
+	{
+		print("AddressableManager_Completed");
+		animation.SetActive(false);
+		cubes = new List<IResourceLocation>(operation.Result);
+
+		print("cubes count:: " + cubes.Count);
+		foreach (var cube in cubes)
+		{
+			Addressables.InstantiateAsync(cube);
+		}
+	}
+
+	#endregion
+}
